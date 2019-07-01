ace84ef88f0fd6e7c5e6a2fed9eea90707dafb2f
Marcelo <mvrozanti@hotmail.com>
2019-07-01 15:32:55 -0300

    fixed Color.MODE

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='colp',  
-     version='1.4',
+     version='1.5',
      author="Marcelo V. Rozanti",
      author_email="mvrozanti@hotmail.com",
      description="Color Processor",
